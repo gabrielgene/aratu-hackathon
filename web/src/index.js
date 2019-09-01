93e829681f50019bc935831cdabbc38226bb44bd
GabrielGene <gabrielgene97@gmail.com>
2019-09-01 00:28:27 -0300

    Add router

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import BaseOnboarding from './components/BaseOnboarding';
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <Route exact path="/" component={BaseOnboarding} />
+    </React.Fragment>
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

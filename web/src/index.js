27861dec63c16677f3d5a8328c196d62eb170ccc
rafael-vitor <rafael.vitor@jusbrasil.com.br>
2019-09-01 00:46:40 -0300

    Fix icons

--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 import BaseOnboarding from './components/BaseOnboarding';
+import Dashboard from './pages/Dashboard';
+
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
+
+library.add(fab, fas)
+
 
 const App = () => (
   <Router>
     <React.Fragment>
       <Route exact path="/" component={BaseOnboarding} />
+      <Route exact path="/dashboard" component={Dashboard} />
     </React.Fragment>
   </Router>
 );

605d64f81427286d3dda6c6fe2720588ce2bce3b
Linzera <italo.lino@ucsal.edu.br>
2019-09-01 00:57:57 -0300

    Add some new behaviors

--- web/src/components/BaseOnboarding/index.js
+++ web/src/components/BaseOnboarding/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Lottie from 'react-lottie';
 import { makeStyles, Typography, CircularProgress } from '@material-ui/core';
 import { GoogleLogin } from 'react-google-login';
@@ -12,27 +12,27 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     padding: 20,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   body: {
     marginTop: 15,
     marginBottom: 30,
     color: 'white',
     fontFamily: 'Ubuntu',
-    textAlign: 'center',
+    textAlign: 'center'
   },
   title: {
     color: 'white',
-    fontFamily: 'Ubuntu',
+    fontFamily: 'Ubuntu'
   },
   progress: {
     margin: theme.spacing(2),
-    color: 'white',
+    color: 'white'
   },
   img: {
     width: theme.spacing(41),
-    marginTop: theme.spacing(2),
-  },
+    marginTop: theme.spacing(2)
+  }
 }));
 
 const defaultLottieOptions = {
@@ -40,8 +40,8 @@ const defaultLottieOptions = {
   autoplay: true,
   animationData: require('../../animations/chicken.json'),
   rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
+    preserveAspectRatio: 'xMidYMid slice'
+  }
 };
 
 export default function BaseOnboarding({ router }) {
@@ -50,7 +50,23 @@ export default function BaseOnboarding({ router }) {
   const [values, setValues] = useState({
     isValidated: false,
     isLoading: false,
+    lat: 0,
+    lng: 0
   });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        setValues({ ...values, lat, lng, isAllowed: true });
+      },
+      () => {
+        setValues({ ...values, isAllowed: false });
+      }
+    );
+  }, []);
 
   const onSuccessGoogle = useCallback(response => {
     const { profileObj } = response;
@@ -58,45 +74,38 @@ export default function BaseOnboarding({ router }) {
 
     setValues({ ...values, isLoading: true });
 
-    navigator.geolocation.getCurrentPosition(position => {
-      const lat = position.coords.latitude;
-      const lng = position.coords.longitude;
+    getAdress(values.lat, values.lng).then(result => {
+      const userData = {
+        email,
+        googleId,
+        givenName,
+        name,
+        familyName,
+        address: result.address
+      };
 
-      getAdress(lat, lng).then(result => {
-        console.log(result.address);
+      //SEND ALL TO BACKEND
+      sendUserData(userData)
+        .then(() => {
+          router.push('/finish');
+        })
+        .catch(err => console.log(err));
 
-        const userData = {
-          email,
-          googleId,
-          givenName,
-          name,
-          familyName,
-          address: result.address,
-        };
-
-        //SEND ALL TO BACKEND
-        sendUserData(userData)
-          .then(() => {
-            router.push('/finish');
-          })
-          .catch(err => console.log(err));
-      });
+      setValues({ isLoading: false, isValidated: true });
     });
   }, []);
 
-  const onFailureGoogle = useCallback(() => {
-    console.log('deu merda');
-  }, []);
+  const onFailureGoogle = useCallback(() => {}, []);
 
   return (
     <div className={classes.root}>
       <img
         className={classes.img}
-        src="https://i.imgur.com/I1NTCFk.png"
-        alt="logo"
+        src='https://i.imgur.com/I1NTCFk.png'
+        alt='logo'
       />
-      <Lottie options={defaultLottieOptions} width="250" height="250" />
-      <Typography className={classes.body} variant="body1">
+      <Lottie options={defaultLottieOptions} width='250' height='250' />
+      <Typography className={classes.body} variant='body1'>
         Estamos participando de um hackaton (competição) e precisamos de sua
         ajuda! Basta logar com o google, e sua ajuda estará computada, ative as
         localizacoes tambem, muito obrigado!
@@ -105,13 +114,13 @@ export default function BaseOnboarding({ router }) {
         {values.isLoading ? (
           <CircularProgress className={classes.progress} />
         ) : values.isValidated ? (
-          <Typography className={classes.title} variant="h4" gutterBottom>
+          <Typography className={classes.title} variant='h4' gutterBottom>
             Obrigado!
           </Typography>
         ) : (
           <GoogleLogin
-            clientId="217118554638-fkh62t3fu61hvlmrqnmeun9idour35jr.apps.googleusercontent.com"
-            buttonText="Entrar com o Google"
+            clientId='217118554638-fkh62t3fu61hvlmrqnmeun9idour35jr.apps.googleusercontent.com'
+            buttonText='Entrar com o Google'
             onSuccess={onSuccessGoogle}
             onFailure={onFailureGoogle}
             cookiePolicy={'single_host_origin'}

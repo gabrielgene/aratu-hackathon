52e7e2bda3bf428315e5706b47aa3dc583a2bdbb
rafael-vitor <rafael.vitor@jusbrasil.com.br>
2019-09-01 01:55:27 -0300

    Improve chart labels

--- web/src/components/HistoryChart/index.js
+++ web/src/components/HistoryChart/index.js
@@ -21,35 +21,35 @@ export default function HistoryChart({ className }) {
     new Chart(document.getElementById("line-chart"), {
       type: "line",
       data: {
-        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+        labels: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90],
         datasets: [
           {
-            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-            label: "Africa",
+            data: [8, 20, 40, 30, 50, 55, 60, 70, 80, 75],
+            label: "Facebook",
             borderColor: "#3e95cd",
             fill: false
           },
           {
-            data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-            label: "Asia",
+            data: [20, 30, 33, 35, 36, 50, 55, 60, 80, 87],
+            label: "Total",
             borderColor: "#8e5ea2",
             fill: false
           },
           {
-            data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-            label: "Europe",
+            data: [8, 10, 20, 30, 33, 35, 40, 40, 41, 42],
+            label: "QR Code",
             borderColor: "#3cba9f",
             fill: false
           },
           {
-            data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
-            label: "Latin America",
+            data: [15, 10, 13, 20, 23, 30, 40, 50, 40, 60],
+            label: "Twitter",
             borderColor: "#e8c3b9",
             fill: false
           },
           {
-            data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
-            label: "North America",
+            data: [10, 10, 13, 20, 23, 30, 40, 42, 50, 60],
+            label: "Instagram",
             borderColor: "#c45850",
             fill: false
           }

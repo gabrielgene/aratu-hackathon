b3341197314865316aac46f496698f51602e705b
Linzera <italo.lino@ucsal.edu.br>
2019-09-01 12:01:15 -0300

    Adding final screen

--- web/src/index.js
+++ web/src/index.js
@@ -11,6 +11,7 @@ import Finish from './pages/Finish';
 import Dashboard from './pages/Dashboard';
 import Data from './pages/Data';
 import FormBuilder from './pages/FormBuilder';
+import InteractionForm from './pages/InteractionForm';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -21,12 +22,13 @@ library.add(fab, fas);
 const App = () => (
   <Router>
     <React.Fragment>
-      <Route exact path="/" component={BaseOnboarding} />
-      <Route path="/concluido" component={Finish} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/formulario" component={Form} />
-      <Route path="/dados" component={Data} />
-      <Route path="/construtor" component={FormBuilder} />
+      <Route exact path='/' component={BaseOnboarding} />
+      <Route path='/concluido' component={Finish} />
+      <Route path='/dashboard' component={Dashboard} />
+      <Route path='/formulario' component={Form} />
+      <Route path='/dados' component={Data} />
+      <Route path='/construtor' component={FormBuilder} />
+      <Route path='/interagir' component={InteractionForm} />
     </React.Fragment>
   </Router>
 );
